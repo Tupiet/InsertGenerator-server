3f6df5c460ae26f350f7b02a35226578b3ccc514
Tupiet <62516852+Tupiet@users.noreply.github.com>
2022-04-07 18:17:20 +0200

    Working - can connect to the server and send and receive JSON data

--- DocumentRoot/index.js
+++ DocumentRoot/index.js
@@ -3,25 +3,72 @@ const dummyjson = require('dummy-json')
 
 const app = express()
 
-const template = `{
-    "name": [
-        {{#repeat 3}}
-        "{{firstName}} {{secondName}}"
-        {{/repeat}}
-    ]
-  }`;  
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
 
-app.get('/', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*')
-    res.set('Content-Type', 'application/json')
-    res.send(dummyjson.parse(`{
-        "name": [
-            {{#repeat ${req.query['quantity']}}}
-            "{{firstName}} {{secondName}}"
-            {{/repeat}}
-        ]
-      }`))
-    console.log(req.query)
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.post('/', (req, res) => {
+    
+    const body = req.body
+
+    let result = manageData(body)
+    res.send(result)
 })
 
-app.listen(80)
+function manageData(response) {
+    let myData = {  }
+
+    for (const element in response.data) {
+        const type = response.data[element]
+
+        switch(type) {
+            case 'Name': 
+                myData[element] = receivedName(element, type, response.info['quantity'])
+                break
+            case 'Street':
+                myData[element] = receivedStreet(element, type, response.info['quantity'])
+                break
+        }
+    }
+    
+    return myData
+}
+
+function receivedName(element, type, quantity) {
+
+    let currentData = dummyjson.parse(`{
+        "data": [
+            {{#repeat ${quantity}}}
+            {
+                "name": "{{firstName}} {{lastName}}"
+            }
+            {{/repeat}}
+        ]
+    }`)
+
+    return JSON.parse(currentData)
+}
+
+function receivedStreet(element, type, quantity) {
+
+    let currentData = dummyjson.parse(`{
+        "data": [
+            {{#repeat ${quantity}}}
+            {
+                "street": "{{street}}"
+            }
+            {{/repeat}}
+        ]
+    }`)
+
+    return JSON.parse(currentData)
+
+}
+
+app.listen(81)
+
